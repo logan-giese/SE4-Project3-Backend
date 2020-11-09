2a9b26b3842e73fa913ad83506a9054202bafcb5
SethKeller <35714713+SethKeller@users.noreply.github.com>
2020-11-09 09:19:54 -0600

    Fixed an issue with the student table
    I found an issue where the Major character length is too short, expanded the limit and updated the data for correctness and consistency.

---
+++ Project 3 Update.sql
@@ -0,0 +1,22 @@
+ALTER TABLE students MODIFY student_major varchar(50);
+UPDATE students
+SET student_major = 'Computer Science with Cybersecurity'
+WHERE student_id = 1545184;
+UPDATE students
+SET student_major = 'Computer Science with Cybersecurity'
+WHERE student_id = 1535722;
+UPDATE students
+SET student_major = 'Computer Science with Gaming and Animation'
+WHERE student_id = 7540307;
+UPDATE students
+SET student_major = 'Computer Science with Gaming and Animation'
+WHERE student_id = 1863897;
+UPDATE students
+SET student_major = 'Computer Science with Gaming and Animation'
+WHERE student_id = 1536632;
+UPDATE students
+SET student_major = 'Computer Science with Gaming and Animation'
+WHERE student_id = 1463678;
+UPDATE students
+SET student_major = 'Computer Science with Gaming and Animation'
+WHERE student_id = 1415454;
